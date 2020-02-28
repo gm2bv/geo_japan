a276e117e5aac63e1ef588c509e711bdbb532cde
gm2bv <gm2bv2001@gmail.com>
2020-02-28 18:17:38 +0900

    APIで測量情報データとGISデータの両方からデータを取得できるように改修

--- api.py
+++ api.py
@@ -21,26 +21,37 @@ def find():
 
     (_pref, _city, num) = get_pref_city(token.words)
     rest_words = token.words[num + 1:]
-
-    pref_city = info[(info[2] == _pref) & (info[3] == _city)]
-    if len(pref_city) != 1:
-        # 該当の都道府県-市区がない場合
-        raise exceptions.NotFound("指定の都道府県・市区が見つかりません")
-
     cnt = len(rest_words)
     if cnt == 0:
         # パラメータ情報が少なすぎ
         raise exceptions.APIException("パラメータ情報が少なすぎです")
-        
-    pref_id = pref_city[0].values[0]
-    city_id = pref_city[1].values[0]
-    pc_path = os.path.join("scrape", "output", "{}_{}.csv.gz".format(pref_id, city_id))
-    if not os.path.exists(pc_path):
-        raise exception.NotFound()
 
+    is_detail = True
+    pref_city = info[(info[2] == _pref) & (info[3] == _city)]
+    if len(pref_city) == 1:
+        pref_id = pref_city[0].values[0]
+        city_id = pref_city[1].values[0]
+        pc_path = os.path.join("scrape", "output", "{}_{}.csv.gz".format(pref_id, city_id))
+        if not os.path.exists(pc_path):
+            raise exception.NotFound()
+    else:
+#        # 該当の都道府県-市区がない場合
+#        raise exceptions.NotFound("指定の都道府県・市区が見つかりません")
+        # tools以下の情報を使う
+        is_detail = False
+        pref_city = info[info[2] == _pref]
+
+        pref_id = pref_city[0].values[0]
+#        city_id = pref_city[1].values[0]
+        pc_path = os.path.join("tools", "output", "{:02}_2018.csv".format(int(pref_id)))
+        if not os.path.exists(pc_path):
+            raise exception.NotFound()
+
+    print("DETAIL", is_detail, dat.shape)
     # データ読込＆解析＆抽出
     # pref, city, town, town_num, words, num1, num2 ,lati, lngi
     dat = pd.read_csv(pc_path, compression='gzip', header=None)
+    dat = dat[(dat[0]==_pref) & (dat[1]==_city)]
     dat['latitude'] = dat[8].astype(float)
     dat['longitude'] = dat[7].astype(float)
 
@@ -56,10 +67,12 @@ def find():
     target = dat[dat[2] == _w]
     if len(rest_words[num+1:]) > 0:
         # 町番号・番地・号の判定
+        ## 半角数字にして分割、リスト化する
         num_infos = "".join(rest_words[num+1:])
         num_infos = mojimoji.zen_to_han(num_infos)
         num_infos = num_infos.replace("丁目", "-")
         num_infos = num_infos.replace("番地", "-")
+        num_infos = num_infos.replace("番", "-")
         num_infos = num_infos.replace("号", "")
         num_infos = num_infos.replace("ー", "-")
         num_infos = num_infos.replace("ｰ", "-")
@@ -67,25 +80,23 @@ def find():
         num_infos = num_infos.split(',')
         while not num_infos[-1]:
             del num_infos[-1]
-        num_infos = [int(num) for num in num_infos]
 
-        if len(num_infos) == 3:
-            target = target[(target[3] == num_infos[0]) & (target[5] == num_infos[1]) & (target[6] == num_infos[2])]
-        elif len(num_infos) == 2:
-            _target = target[(target[3] == num_infos[0]) & (target[5] == num_infos[1])]
-            if len(_target) == 0:
-                _target = target[(target[5] == num_infos[0]) & (target[6] == num_infos[1])]
-            target = _target
-        elif len(num_infos) == 1:
-            _target = target[(target[3] == num_infos[0])]
-            if len(_target) == 0:
-                _target = target[(target[5] == num_infos[0])]
-            if len(_target) == 0:
-                _target = target[(target[6] == num_infos[0])]
-            target = _target                        
+        num_infos = [int(num) for num in num_infos]
+        _target = []
+        i = 0
+        while len(_target) == 0:
+            _infos = num_infos[:i] if i < 0 else num_infos
+            if len(_infos) <= 0:
+                break
+            _target = select_target(target, _infos)
+            i -= 1
+
+        target = _target
+        hit_infos = _infos
     else:
         # 番地・号がない場合
         num_infos = None
+        hit_infos = None
 
     if len(target) == 1:
         latitude = target['latitude'].values[0]
@@ -101,6 +112,7 @@ def find():
         "city": _city,
         "town": _w,
         "infos": num_infos,
+        "hit_infos": hit_infos,
         "latitude": latitude,
         "longitude": longitude,
     }
@@ -143,6 +155,26 @@ def get_pref_city(words):
 
         num += 1
     return (_pref, _city, num)
+
+
+def select_target(target, num_infos):
+    if len(num_infos) == 3:
+        _target = target[(target[3] == num_infos[0]) & (target[5] == num_infos[1]) & (target[6] == num_infos[2])]
+    elif len(num_infos) == 2:
+        _target = target[(target[3] == num_infos[0]) & (target[5] == num_infos[1])]
+        if len(_target) == 0:
+            _target = target[(target[5] == num_infos[0]) & (target[6] == num_infos[1])]
+    elif len(num_infos) == 1:
+        _target = target[(target[3] == num_infos[0])]
+        if len(_target) == 0:
+            _target = target[(target[5] == num_infos[0])]
+        if len(_target) == 0:
+            _target = target[(target[6] == num_infos[0])]
+    else:
+        # あり得ない分岐
+        raise Exception("num_infosが不正です")
+
+    return _target
     
 
 if __name__ == "__main__":
